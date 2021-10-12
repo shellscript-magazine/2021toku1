60227284e5a2720a519889326325f1561fd20fc5
Shell Script Magazine <35516434+shellscript-magazine@users.noreply.github.com>
2021-10-12 23:17:18 +0900

    Update ubuntu_init2.sh

--- ubuntu_init2.sh
+++ ubuntu_init2.sh
@@ -2,6 +2,7 @@
 
 ##固定IPアドレス
 IP_ADDRESS="192.168.10.100"
+GATEWAY_IPV4="192.168.10.1"
 
 ##旧設定バックアップ
 mkdir -p ~/old_settings
@@ -14,12 +15,13 @@ network:
     eth0:
       dhcp4: false
       addresses: [ip_address/24]
-      gateway4: 192.168.10.1
+      gateway4: gateway_ipv4
       nameservers:
         addresses: [8.8.8.8]
   version: 2
 EOF
 sudo sed -i -e "s%ip_address%$IP_ADDRESS%" /etc/netplan/50-cloud-init.yaml
+sudo sed -i -e "s%gateway_ipv4%$GATEWAY_IPV4%" /etc/netplan/50-cloud-init.yaml
 
 ##ネットワーク設定反映
 sudo netplan apply
